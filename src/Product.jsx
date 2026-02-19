fc52fe01d89cb4e485f403917ebb729d94d06bbe
Abhishek Agrawal <abhishekagrawal946@gmail.com>
2026-02-19 19:19:51 +0530

    your commit message

--- src/Product.jsx
+++ src/Product.jsx
@@ -7,67 +7,67 @@ function Product() {
     useEffect(() => {
         dispatch(fetchProducts())
     }, []);
-    const Productselector =useSelector((state) => state.product.items)
+    const Productselector = useSelector((state) => state.product.items)
 
     console.log(Productselector);
     const cartItems = useSelector((state) => state.cart.items)
     const CartLength = (cartItems.length)
     return (
-    <>
-        <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        🛍 Product Store
-      </h1>
+        <>
+            <div className="min-h-screen bg-gray-100 p-6">
+                <h1 className="text-3xl font-bold text-center mb-8">
+                    🛍 Product Store
+                </h1>
 
-<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">            {
-             Productselector.length && Productselector.map((item) =>(
-                   <div
-            key={item.id}
-            className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden"
-          >
-            <img
-              src={item.thumbnail}
-              alt={item.title}
-              className="h-48 w-full object-cover"
-            />
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">            {
+                    Productselector.length && Productselector.map((item) => (
+                        <div
+                            key={item.id}
+                            className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden"
+                        >
+                            <img
+                                src={item.thumbnail}
+                                alt={item.title}
+                                className="h-48 w-full object-cover"
+                            />
 
-            <div className="p-4">
-              <h2 className="font-semibold text-lg line-clamp-1">
-                {item.title}
-              </h2>
+                            <div className="p-4">
+                                <h2 className="font-semibold text-lg line-clamp-1">
+                                    {item.title}
+                                </h2>
 
-              <p className="text-sm text-gray-500 capitalize">
-                {item.category}
-              </p>
+                                <p className="text-sm text-gray-500 capitalize">
+                                    {item.category}
+                                </p>
 
-              <div className="flex justify-between items-center mt-3">
-                <span className="text-green-600 font-bold text-lg">
-                  $ {item.price}
-                </span>
+                                <div className="flex justify-between items-center mt-3">
+                                    <span className="text-green-600 font-bold text-lg">
+                                        $ {item.price}
+                                    </span>
 
-                <span className="text-yellow-500 text-sm">
-                  ⭐ {item.rating}
-                </span>
-              </div>
+                                    <span className="text-yellow-500 text-sm">
+                                        ⭐ {item.rating}
+                                    </span>
+                                </div>
 
-{
-cartItems.find(cartItem=>cartItem.id === item.id) ?
-<button className="mt-4 w-full bg-green-400 text-white py-2 rounded-xl hover:bg-gray-800 transition" >
-                Added to Cart
-              </button>
-              : <button className="mt-4 w-full bg-blue-400 text-white py-2 rounded-xl hover:bg-gray-800 transition" onClick={()=>dispatch(addItem(item))}>
-                Add to Cart
-              </button>
-}
-              
+                                {
+                                    cartItems.find(cartItem => cartItem.id === item.id) ?
+                                        <button className="mt-4 w-full bg-red-600 text-white py-2 rounded-xl hover:bg-gray-800 transition" onClick={() => dispatch(removeItem(item))}>
+                                            Remove from Cart
+                                        </button>
+                                        : <button className="mt-4 w-full bg-blue-400 text-white py-2 rounded-xl hover:bg-gray-800 transition" onClick={() => dispatch(addItem(item))}>
+                                            Add to Cart
+                                        </button>
+                                }
+
+                            </div>
+                        </div>
+                    ))
+                }
+                </div>
             </div>
-          </div>
-             ))   
-            }
-            </div>
-            </div>
-</>
-       
+        </>
+
     );
 }
 
--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,26 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-items : []
-}
+  items: JSON.parse(localStorage.getItem("cart")) || []
+};
 
 const addToCart = createSlice({
-    name : "cart",
+    name: "cart",
     initialState,
-    reducers:{
-        addItem: (state , action) =>{
-           state.items.push(action.payload);
-            
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+            localStorage.setItem("cart", JSON.stringify(state.items));
         },
-        removeItem:(state) =>{
-           state.value> 0 ? state.value-=1 : 0;
+        removeItem: (state , action) => {
+           const cartData = state.items.filter((item) => item.id != action.payload.id);
+           state.items=cartData;
+           localStorage.setItem("cart",JSON.stringify(cartData))
+         },
+        clearAllItem: (state) => {
+            state.value = 0;
         },
-          clearAllItem:(state) =>{
-           state.value=  0;
-        },
-        
+
 
     }
 });
-export const {addItem , removeItem , clearAllItem  } = addToCart.actions;
+export const { addItem, removeItem, clearAllItem } = addToCart.actions;
 export default addToCart.reducer
